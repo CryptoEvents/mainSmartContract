9ef45e189da689eecb765da785c2d1286551af03
Andrey <systerr@gmail.com>
2017-12-09 15:57:25 +0100

    reformat code

--- contracts/mainSmartContract.sol
+++ contracts/mainSmartContract.sol
@@ -3,44 +3,44 @@ pragma solidity ^0.4.18;
 import "./ERC20Token.sol";
 
 contract mainSmartContract {
-  address public owner;
-  uint public fees;
-  ERC20Token etoken;
-  
-  struct smartEvent {
-      string name;
-      string symbol;
-      uint decimals;
-      address contractEvent;
-  }
-  
-  mapping(address => smartEvent) public smartevents;
-  
-  modifier onlyOwner() {
-    require(msg.sender == owner);
-    _;
-  }
-  
-  function mainSmartContract(uint8 _fees) public {
-    owner = msg.sender;
-    fees = _fees;
-  }
+    address public owner;
+    uint public fees;
+    ERC20Token etoken;
 
-  function changeFees(uint8 _fees) onlyOwner public {
-    fees = _fees;
-  }
-  
-  function deployNew(string _name,string _symbol,uint8 _decimals) payable {
-    require(msg.value>fees);
-    smartevents[msg.sender].name=_name;
-    smartevents[msg.sender].symbol=_symbol;
-    smartevents[msg.sender].decimals=_decimals;
-    smartevents[msg.sender].contractEvent=new ERC20Token(msg.sender);
-  }
-  
-  function getContract() constant public
-          returns (smartEvent contracts)
-  {
-      contracts = smartevents[msg.sender];
-  }
+    struct smartEvent {
+        string name;
+        string symbol;
+        uint decimals;
+        address contractEvent;
+    }
+
+    mapping(address => smartEvent) public smartevents;
+
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
+    function mainSmartContract(uint8 _fees) public {
+        owner = msg.sender;
+        fees = _fees;
+    }
+
+    function changeFees(uint8 _fees) onlyOwner public {
+        fees = _fees;
+    }
+
+    function deployNew(string _name, string _symbol, uint8 _decimals) payable {
+        require(msg.value > fees);
+        smartevents[msg.sender].name = _name;
+        smartevents[msg.sender].symbol = _symbol;
+        smartevents[msg.sender].decimals = _decimals;
+        smartevents[msg.sender].contractEvent = new ERC20Token(msg.sender);
+    }
+
+    function getContract() constant public
+    returns (smartEvent contracts)
+    {
+        contracts = smartevents[msg.sender];
+    }
 }
